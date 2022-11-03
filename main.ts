98bd44b426f7e227a1bd82e1c8a91323102d9555
200mill <97425242+200mill@users.noreply.github.com>
2022-11-03 15:44:55 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        pins.servoWritePin(AnalogPin.P1, 0)
+    } else if (receivedNumber == 1) {
+        pins.servoWritePin(AnalogPin.P1, 180)
+    } else if (receivedNumber == 2) {
+        pins.servoWritePin(AnalogPin.P1, 90)
+    }
+})
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P1, 90)
-})
-input.onButtonPressed(Button.AB, function () {
-    pins.servoWritePin(AnalogPin.P1, 0)
-})
-input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P1, 180)
 })
+input.onButtonPressed(Button.AB, function () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P1, 0)
+})
+radio.setGroup(1)
 basic.forever(function () {
 	
 })
