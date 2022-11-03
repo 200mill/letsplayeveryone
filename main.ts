086b625a7417b0c7d4e5debf6a17076e0092a9e1
200mill <97425242+200mill@users.noreply.github.com>
2022-11-03 16:57:06 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
+let loadtime = 0
+let time = 0
 OLED.init(128, 64)
+for (let index = 0; index < 100; index++) {
+    OLED.drawLoading(time)
+    time += 1
+}
 basic.forever(function () {
+    OLED.writeNumNewLine(loadtime)
     OLED.writeStringNewLine("Sensor1 : " + Environment.ReadSoilHumidity(AnalogPin.P1))
+    loadtime += 1
     OLED.clear()
 })
