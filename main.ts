114d8dc35bf343eaba9a67aa408eb6f71b92717b
200mill <97425242+200mill@users.noreply.github.com>
2022-11-03 16:24:54 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,33 +1,12 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-        music.ringTone(131)
-        pins.servoWritePin(AnalogPin.P1, 0)
-    } else if (receivedNumber == 1) {
-        music.ringTone(500)
-        pins.servoWritePin(AnalogPin.P1, 180)
-    } else if (receivedNumber == 2) {
-        music.ringTone(1000)
-        pins.servoWritePin(AnalogPin.P1, 90)
-    }
-})
 input.onButtonPressed(Button.A, function () {
-    music.ringTone(500)
-    pins.servoWritePin(AnalogPin.P1, 180)
-})
-input.onButtonPressed(Button.AB, function () {
-    pins.servoWritePin(AnalogPin.P1, 90)
-    music.ringTone(1000)
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    pins.servoWritePin(AnalogPin.P1, randint(0, 180))
 })
 input.onButtonPressed(Button.B, function () {
-    music.ringTone(131)
+    music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
     pins.servoWritePin(AnalogPin.P1, 0)
 })
 pins.setAudioPin(AnalogPin.P10)
-OLED.init(128, 64)
 basic.forever(function () {
-    radio.setGroup(randint(0, 255))
-    basic.pause(10000)
-})
-basic.forever(function () {
-    OLED.writeStringNewLine("")
+	
 })
