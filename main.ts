381b3f649581211a37d6c0d01a71aca2d5b07a2b
200mill <97425242+200mill@users.noreply.github.com>
2022-11-03 15:51:35 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,22 +1,33 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
+        music.ringTone(131)
         pins.servoWritePin(AnalogPin.P1, 0)
     } else if (receivedNumber == 1) {
+        music.ringTone(500)
         pins.servoWritePin(AnalogPin.P1, 180)
     } else if (receivedNumber == 2) {
+        music.ringTone(1000)
         pins.servoWritePin(AnalogPin.P1, 90)
     }
 })
 input.onButtonPressed(Button.A, function () {
+    music.ringTone(500)
     pins.servoWritePin(AnalogPin.P1, 180)
 })
 input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P1, 90)
+    music.ringTone(1000)
 })
 input.onButtonPressed(Button.B, function () {
+    music.ringTone(131)
     pins.servoWritePin(AnalogPin.P1, 0)
 })
+pins.setAudioPin(AnalogPin.P10)
+OLED.init(128, 64)
 basic.forever(function () {
     radio.setGroup(randint(0, 255))
     basic.pause(10000)
+})
+basic.forever(function () {
+    OLED.writeStringNewLine("")
 })
