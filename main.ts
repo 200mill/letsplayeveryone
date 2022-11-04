52411a0aeb2d8b3fdc43172d775e44f1e5b09d68
200mill <97425242+200mill@users.noreply.github.com>
2022-11-04 17:02:38 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -20,7 +20,13 @@ function 왼쪽 () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
 }
 input.onButtonPressed(Button.A, function () {
-	
+    radio.sendNumber(2)
+})
+input.onGesture(Gesture.FreeFall, function () {
+    radio.sendNumber(5)
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendNumber(4)
 })
 function 오른쪽 () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
@@ -33,12 +39,18 @@ function 멈춰 () {
     maqueen.motorStop(maqueen.Motors.All)
 }
 input.onButtonPressed(Button.AB, function () {
-	
+    radio.sendNumber(1)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    radio.sendNumber(3)
 })
 function 앞으로 () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 151)
 }
 radio.setGroup(247)
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 30) {
+        멈춰()
+        music.playTone(800, music.beat(BeatFraction.Double))
+    }
+})
