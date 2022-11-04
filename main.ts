33d1307275e0ab805e066d423f3baf8db58d4b60
200mill <97425242+200mill@users.noreply.github.com>
2022-11-04 16:08:25 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+let SPD = 0
+input.onButtonPressed(Button.A, function () {
+    SPD += 10
+})
+IR.IR_callbackUser(function (message) {
+    if (message == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, SPD)
+    } else if (message == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, SPD)
+    } else if (message == 2) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, SPD)
+    } else if (message == 3) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, SPD)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    SPD = 30
+})
+input.onButtonPressed(Button.B, function () {
+    SPD += -10
+})
 basic.forever(function () {
-	
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 20) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
