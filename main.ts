42f1fc2833efb267f0416a9dd56ea0a09fa3d71b
200mill <97425242+200mill@users.noreply.github.com>
2022-11-04 15:35:00 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,13 +1,1 @@
-let loadtime = 0
-let time = 0
-OLED.init(128, 64)
-for (let index = 0; index < 100; index++) {
-    OLED.drawLoading(time)
-    time += 1
-}
-basic.forever(function () {
-    OLED.writeNumNewLine(loadtime)
-    OLED.writeStringNewLine("Sensor1 : " + Environment.ReadDust(DigitalPin.P13, AnalogPin.P1))
-    loadtime += 1
-    OLED.clear()
-})
+
