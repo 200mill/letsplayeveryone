9352c034bb198ba6afaf011d59c252598b5ecebd
200mill <97425242+200mill@users.noreply.github.com>
2022-11-12 11:06:19 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
     basic.pause(1000)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 100)
     basic.pause(1000)
 })
