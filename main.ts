905d5f771de3050e57ede70283ecb09267eb1b7a
200mill <97425242+200mill@users.noreply.github.com>
2022-11-12 11:18:24 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,17 +1,22 @@
-input.onButtonPressed(Button.A, function () {
+function turnleft () {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
     basic.pause(1000)
     maqueen.motorStop(maqueen.Motors.All)
-})
-input.onButtonPressed(Button.AB, function () {
+}
+function backward () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
     basic.pause(1000)
     maqueen.motorStop(maqueen.Motors.All)
-})
-input.onButtonPressed(Button.B, function () {
+}
+function turnright () {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 100)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
     basic.pause(1000)
     maqueen.motorStop(maqueen.Motors.All)
-})
+}
+function foward () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    basic.pause(1000)
+    maqueen.motorStop(maqueen.Motors.All)
+}
